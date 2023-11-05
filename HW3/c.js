7e98e80631969e2534d0975e56d0a13e98c8fe06
N Wang <nw128@duke.edu>
2023-11-04 23:53:20 -0400

    c done

---
+++ HW3/c.js
@@ -0,0 +1,28 @@
+printjson(db.people.aggregate([
+    { $replaceRoot: {
+        newRoot: {
+            name: "$name",
+            age: {$subtract: [2023, {$year: "$birthday"}]},
+            gender: "$gender",
+            state: {
+                $arrayElemAt: [ "$roles.state", -1 ]
+            },
+            type: {
+                $arrayElemAt: [ "$roles.type", -1 ]
+            },
+            party: {
+                $arrayElemAt: [ "$roles.party", -1 ]
+            },
+        }
+    } },
+    {
+        $match: {
+            "party": "Democrat",
+            "gender": "F",
+        }
+    },
+    { $sort: {
+        age: -1,
+        name: 1
+    } },
+]).toArray())
